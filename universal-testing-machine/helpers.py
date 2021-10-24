791e857436dff24b4dfef971cca6a017c50c43f0
FrancescoNegri <francesco.negri@outlook.com>
2021-10-24 16:47:13 +0200

    Refactor data table with live display

--- universal-testing-machine/helpers.py
+++ universal-testing-machine/helpers.py
@@ -4,6 +4,7 @@ from InquirerPy import inquirer, validator
 from rich import box
 from rich.console import Console
 from rich.table import Table
+from rich.live import Live
 console = Console()
 from datetime import datetime
 from utility import utility
@@ -130,7 +131,7 @@ def adjust_crossbar_position(my_controller:controller.LinearController, adjustme
 
     return
 
-def _show_data_table(force:float, absolute_position:float, printed_lines:int):
+def _generate_data_table(force:float, absolute_position:float):
     if force is None:
         force = '-'
     else:
@@ -145,11 +146,8 @@ def _show_data_table(force:float, absolute_position:float, printed_lines:int):
     table.add_column('Force', justify='center', min_width=12)
     table.add_column('Absolute position', justify='center', min_width=20)
     table.add_row(f'{force} N', f'{absolute_position} mm')
-    
-    console.print(table)
-    printed_lines += 5
 
-    return printed_lines
+    return table
 
 def start_manual_mode(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, speed:float, mode_button_pin:int, up_button_pin:int, down_button_pin:int):
     mode = 0
@@ -179,37 +177,32 @@ def start_manual_mode(my_controller:controller.LinearController, my_loadcell:loa
     absolute_position = None
     batch_index = 0
     batch_size = 25
+
+    live_table = Live(_generate_data_table(force, absolute_position), refresh_per_second=12, transient=True)
     
-    while mode == 0:
-        if printed_lines > 1:
-            utility.delete_last_lines(printed_lines - 1)
-            printed_lines -= printed_lines - 1
-        
-        if my_loadcell.is_calibrated:
-            while my_loadcell.is_batch_ready(batch_index, batch_size):                
-                batch, batch_index = my_loadcell.get_batch(batch_index, batch_size)
-                force = mean(batch['F'])
-        else:
-            force = None
+    with live_table:
+        while mode == 0:            
+            if my_loadcell.is_calibrated:
+                while my_loadcell.is_batch_ready(batch_index, batch_size):                
+                    batch, batch_index = my_loadcell.get_batch(batch_index, batch_size)
+                    force = mean(batch['F'])
+            else:
+                force = None
 
-        if my_controller.is_calibrated:
-            try:
-                absolute_position = my_controller.get_absolute_position()
-            except:
+            if my_controller.is_calibrated:
+                try:
+                    absolute_position = my_controller.get_absolute_position()
+                except:
+                    absolute_position = None
+            else:
                 absolute_position = None
-        else:
-            absolute_position = None
 
-        printed_lines = _show_data_table(
-            force=force,
-            absolute_position=absolute_position,
-            printed_lines=printed_lines
-        )
+            live_table.update(_generate_data_table(force, absolute_position))
 
-        if down_button.is_active and my_controller._down_endstop.is_active:
-            my_controller.motor_stop()
-        if up_button.is_active and my_controller._up_endstop.is_active:
-            my_controller.motor_stop()
+            if down_button.is_active and my_controller._down_endstop.is_active:
+                my_controller.motor_stop()
+            if up_button.is_active and my_controller._up_endstop.is_active:
+                my_controller.motor_stop()
 
     if my_loadcell.is_calibrated:
         my_loadcell.stop_reading()
@@ -348,37 +341,37 @@ def _start_monotonic_test(my_controller:controller.LinearController, my_loadcell
     fig.canvas.draw()
     plt.show(block=False)
 
+    live_table = Live(_generate_data_table(None, None), refresh_per_second=12, transient=True)
+
     _, _, t0 = my_controller.run(linear_speed, displacement, controller.UP)
     my_loadcell.start_reading()
 
-    while my_controller.is_running:
-        if stop_flag:
-            my_controller.abort()
-        else:
-            while my_loadcell.is_batch_ready(batch_index):
-                batch, batch_index = my_loadcell.get_batch(batch_index)
-                batch['t'] = batch['t'] - t0
-                batch['strain'] = (batch['t'] * linear_speed / initial_gauge_length) * 100
-
-                forces.extend(batch['F'])
-                strains.extend(batch['strain'])
-
-                line.set_data(strains, forces)
-                ax.redraw_in_frame()
-                fig.canvas.blit(ax.bbox)
-                fig.canvas.flush_events()
+    with live_table:
+        while my_controller.is_running:
+            if stop_flag:
+                my_controller.abort()
             else:
-                pass
-                
-            if printed_lines > 1:
-                utility.delete_last_lines(printed_lines - 1)
-                printed_lines -= printed_lines - 1
-            
-            printed_lines = _show_data_table(
-                force=forces[-1] if len(forces) > 0 else None,
-                absolute_position=(initial_absolute_position + (strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
-                printed_lines=printed_lines
-            )
+                while my_loadcell.is_batch_ready(batch_index):
+                    batch, batch_index = my_loadcell.get_batch(batch_index)
+                    batch['t'] = batch['t'] - t0
+                    batch['strain'] = (batch['t'] * linear_speed / initial_gauge_length) * 100
+
+                    forces.extend(batch['F'])
+                    strains.extend(batch['strain'])
+
+                    line.set_data(strains, forces)
+                    ax.redraw_in_frame()
+                    fig.canvas.blit(ax.bbox)
+                    fig.canvas.flush_events()
+                else:
+                    pass
+                    
+                live_table.update(
+                    _generate_data_table(
+                        force=forces[-1] if len(forces) > 0 else None, 
+                        absolute_position=(initial_absolute_position + (strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None
+                    )
+                )
 
     utility.delete_last_lines(printed_lines)
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
@@ -430,40 +423,40 @@ def _start_static_test(my_controller:controller.LinearController, my_loadcell:lo
     fig.canvas.draw()
     plt.show(block=False)
 
+    live_table = Live(_generate_data_table(None, None), refresh_per_second=12, transient=True)
+
     t0 = my_controller.hold_torque()
     my_loadcell.start_reading()
 
-    while my_controller.is_running:
-        if stop_flag:
-            my_controller.release_torque()
-        else:
-            while my_loadcell.is_batch_ready(batch_index):
-                batch, batch_index = my_loadcell.get_batch(batch_index)
-                batch['t'] = batch['t'] - t0
-
-                forces.extend(batch['F'])
-                timings.extend(batch['t'])
-
-                if batch['t'].iloc[-1] > xlim:
-                    ax.set_xlim([(xlim / 2), (xlim / 2) + batch['t'].iloc[-1]])
-                    xlim = (xlim / 2) + batch['t'].iloc[-1]
-
-                line.set_data(timings, forces)
-                ax.redraw_in_frame()
-                fig.canvas.blit(ax.bbox)
-                fig.canvas.flush_events()
+    with live_table:
+        while my_controller.is_running:
+            if stop_flag:
+                my_controller.release_torque()
             else:
-                pass
-        
-        if printed_lines > 1:
-            utility.delete_last_lines(printed_lines - 1)
-            printed_lines -= printed_lines - 1
-        
-        printed_lines = _show_data_table(
-            force=forces[-1] if len(forces) > 0 else None,
-            absolute_position=None,
-            printed_lines=printed_lines
-        )
+                while my_loadcell.is_batch_ready(batch_index):
+                    batch, batch_index = my_loadcell.get_batch(batch_index)
+                    batch['t'] = batch['t'] - t0
+
+                    forces.extend(batch['F'])
+                    timings.extend(batch['t'])
+
+                    if batch['t'].iloc[-1] > xlim:
+                        ax.set_xlim([(xlim / 2), (xlim / 2) + batch['t'].iloc[-1]])
+                        xlim = (xlim / 2) + batch['t'].iloc[-1]
+
+                    line.set_data(timings, forces)
+                    ax.redraw_in_frame()
+                    fig.canvas.blit(ax.bbox)
+                    fig.canvas.flush_events()
+                else:
+                    pass
+
+                live_table.update(
+                    _generate_data_table(
+                        force=forces[-1] if len(forces) > 0 else None,
+                        absolute_position=None
+                    )
+                )
 
     utility.delete_last_lines(printed_lines)
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
