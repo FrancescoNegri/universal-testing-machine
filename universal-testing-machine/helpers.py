c3d5d3598c4531a0f4681a3bf76d2f1bb00b6b03
FrancescoNegri <francesco.negri@outlook.com>
2021-10-26 21:58:57 +0200

    Refactor read parameters function

--- universal-testing-machine/helpers.py
+++ universal-testing-machine/helpers.py
@@ -228,6 +228,52 @@ def start_manual_mode(my_controller:controller.LinearController, my_loadcell:loa
     
     return
 
+def _read_monotonic_test_parameters(default_clamps_distance:float = None):
+    test_parameters = {}
+
+    test_parameters['cross_section'] = {
+                'value': float(
+                    inquirer.text(
+                        message='Insert the sample cross section [mm²]:',
+                        validate=validator.NumberValidator(float_allowed=True)
+                    ).execute()
+                ),
+                'unit': 'mm²'
+            }
+
+    test_parameters['displacement'] = {
+        'value': float(
+            inquirer.text(
+                message='Insert the desired displacement [mm]:',
+                validate=validator.NumberValidator(float_allowed=True)
+            ).execute()
+        ),
+        'unit': 'mm'
+    }
+
+    test_parameters['linear_speed'] = {
+        'value': float(
+            inquirer.text(
+                message='Insert the desired linear speed [mm/s]:',
+                validate=validator.NumberValidator(float_allowed=True)
+            ).execute()
+        ),
+        'unit': 'mm/s'
+    }
+
+    test_parameters['clamps_distance'] = {
+        'value': float(
+            inquirer.text(
+                message='Insert the clamps distance [mm]:',
+                validate=validator.NumberValidator(float_allowed=True),
+                default=str(default_clamps_distance)
+            ).execute()
+        ),
+        'unit': 'mm'
+    }
+
+    return test_parameters
+
 def read_test_parameters(test_type:bool, default_clamps_distance:float = None):
     is_confirmed = False
 
@@ -240,46 +286,11 @@ def read_test_parameters(test_type:bool, default_clamps_distance:float = None):
 
     while not is_confirmed:
         if test_type is 'monotonic':        
-            test_parameters['cross_section'] = {
-                'value': float(
-                    inquirer.text(
-                        message='Insert the sample cross section [mm²]:',
-                        validate=validator.NumberValidator(float_allowed=True)
-                    ).execute()
-                ),
-                'unit': 'mm²'
-            }
-
-            test_parameters['displacement'] = {
-                'value': float(
-                    inquirer.text(
-                        message='Insert the desired displacement [mm]:',
-                        validate=validator.NumberValidator(float_allowed=True)
-                    ).execute()
-                ),
-                'unit': 'mm'
-            }
-
-            test_parameters['linear_speed'] = {
-                'value': float(
-                    inquirer.text(
-                        message='Insert the desired linear speed [mm/s]:',
-                        validate=validator.NumberValidator(float_allowed=True)
-                    ).execute()
-                ),
-                'unit': 'mm/s'
-            }
-
-            test_parameters['clamps_distance'] = {
-                'value': float(
-                    inquirer.text(
-                        message='Insert the clamps distance [mm]:',
-                        validate=validator.NumberValidator(float_allowed=True),
-                        default=str(default_clamps_distance)
-                    ).execute()
-                ),
-                'unit': 'mm'
-            }
+            test_parameters = {**test_parameters, **_read_monotonic_test_parameters(default_clamps_distance)}
+        elif test_type is 'cyclic':
+            pass
+        elif test_type is 'static':
+            pass
 
         test_parameters['test_id'] = inquirer.text(
             message='Insert the ID for this session:',
@@ -314,6 +325,96 @@ def save_test_parameters(my_controller:controller.LinearController, my_loadcell:
     return
 
 def _start_monotonic_test(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, test_parameters:dict, stop_button_pin:int):
+    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...')
+    printed_lines = 1
+    
+    displacement = test_parameters['displacement']['value']
+    linear_speed = test_parameters['linear_speed']['value']
+    cross_section = test_parameters['cross_section']['value']
+    initial_gauge_length = test_parameters['initial_gauge_length']['value']
+    initial_absolute_position = my_controller.get_absolute_position()
+    loadcell_limit = my_loadcell.get_calibration()['loadcell_limit']['value']
+
+    stop_flag = False
+    def _switch_stop_flag():
+        nonlocal stop_flag
+        stop_flag = True
+        return
+
+    stop_button = Button(pin=stop_button_pin)
+    stop_button.when_released = lambda: _switch_stop_flag()
+
+    strains = []
+    forces = []
+    batch_index = 0
+
+    fig = plt.figure(facecolor='#DEDEDE')
+    ax = plt.axes()
+    line, = ax.plot(forces, strains, lw=3)
+
+    xlim = round((displacement / initial_gauge_length) * 1.1 * 100) # 10% margin
+    ylim = loadcell_limit
+    ax.set_xlim([0, xlim])
+    ax.set_ylim([0, ylim])
+    ax.set_xlabel('Strain (%)')
+    ax.set_ylabel('Force (N)')
+    ax.set_title('Force vs. Strain')
+    
+    fig.canvas.draw()
+    plt.show(block=False)
+
+    live_table = Live(_generate_data_table(None, None, None), refresh_per_second=12, transient=True)
+
+    _, _, t0 = my_controller.run(linear_speed, displacement, controller.UP)
+    my_loadcell.start_reading()
+
+    with live_table:
+        while my_controller.is_running:
+            if stop_flag:
+                my_controller.abort()
+            else:
+                while my_loadcell.is_batch_ready(batch_index):
+                    batch, batch_index = my_loadcell.get_batch(batch_index)
+                    batch['t'] = batch['t'] - t0
+                    batch['strain'] = (batch['t'] * linear_speed / initial_gauge_length) * 100
+
+                    forces.extend(batch['F'])
+                    strains.extend(batch['strain'])
+
+                    line.set_data(strains, forces)
+                    ax.redraw_in_frame()
+                    fig.canvas.blit(ax.bbox)
+                    fig.canvas.flush_events()
+                else:
+                    pass
+                    
+                live_table.update(
+                    _generate_data_table(
+                        force=forces[-1] if len(forces) > 0 else None, 
+                        absolute_position=(initial_absolute_position + (strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
+                        loadcell_limit=loadcell_limit
+                    )
+                )
+
+    utility.delete_last_lines(printed_lines)
+    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
+
+    data = my_loadcell.stop_reading()
+    stop_button.when_released = None
+
+    data['t'] = data['t'] - t0
+    data['displacement'] = data['t'] * linear_speed
+    data['F_raw'] = data['F']
+    data['F_med20'] = scipy.signal.medfilt(data['F'], 21)
+    data['stress_raw'] = data['F_raw'] / cross_section
+    data['stress_med20'] = data['F_med20'] / cross_section
+    data['strain'] = (data['t'] * linear_speed / initial_gauge_length) * 100
+    data.loc[data.index[0], 'cross_section'] = cross_section
+    data.loc[data.index[0], 'initial_gauge_length'] = initial_gauge_length
+
+    return data
+
+def _start_cyclic_test(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, test_parameters:dict, stop_button_pin:int):
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...')
     printed_lines = 1
     
@@ -496,7 +597,12 @@ def start_test(my_controller:controller.LinearController, my_loadcell:loadcell.L
             stop_button_pin=stop_button_pin
         )
     elif test_parameters['test_type'] is 'cyclic':
-        pass
+        data = _start_cyclic_test(
+            my_controller=my_controller,
+            my_loadcell=my_loadcell,
+            test_parameters=test_parameters,
+            stop_button_pin=stop_button_pin
+        )
     elif test_parameters['test_type'] is 'static':
         data = _start_static_test(
             my_controller=my_controller,
