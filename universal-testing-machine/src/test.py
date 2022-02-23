10175a131b6d0b0d81460c797bd24dcd30238f18
FrancescoNegri <francesco.negri@outlook.com>
2022-02-23 22:33:04 +0100

    Fix Tests get stuck after a while #16

--- universal-testing-machine/src/test.py
+++ universal-testing-machine/src/test.py
@@ -13,14 +13,23 @@ from controller import controller
 from loadcell import loadcell
 from src import constants, helpers
 
+def _init_plot_data(plot_item, plot_color):
+    plot_data = plot_item.plot(
+    pen=None, symbol=constants.PLOTS_SYMBOL, symbolSize=constants.PLOTS_SYMBOL_SIZE)
+    plot_data.opts['useCache'] = True
+    plot_data.setSymbolPen(mkPen(plot_color))
 
-def _run_go(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, plot_item, plot_color, speed, displacement, direction, stop_flag, stop_button, initial_absolute_position, reference_absolute_position, test_parameters):
+    forces = []
+    strains = []
+
+    return plot_data, forces, strains
+
+def _run_go(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, plot_item, plot_color, speed, displacement, direction, stop_flag, stop_button, initial_absolute_position, reference_absolute_position, test_parameters):
     data = None
 
     if stop_flag is False:
         initial_gauge_length = test_parameters['initial_gauge_length']['value']
-        loadcell_limit = my_loadcell.get_calibration()[
-            'loadcell_limit']['value']
+        loadcell_limit = my_loadcell.get_calibration()['loadcell_limit']['value']
 
         stop_flag = stop_flag
 
@@ -30,17 +39,10 @@ def _run_go(my_controller: controller.LinearController, my_loadcell: loadcell.Lo
             return
         stop_button.when_released = lambda: _switch_stop_flag()
 
-        plot_data = plot_item.plot(
-            pen=None, symbol=constants.PLOTS_SYMBOL, symbolSize=constants.PLOTS_SYMBOL_SIZE)
-        plot_data.opts['useCache'] = True
-        plot_data.setSymbolPen(mkPen(plot_color))
-
-        strains = []
-        forces = []
+        plot_data, forces, strains = _init_plot_data(plot_item, plot_color)
         batch_index = 0
 
-        live_table = Live(helpers.generate_data_table(
-            None, None, None, None), refresh_per_second=12, transient=True)
+        live_table = Live(helpers.generate_data_table(None, None, None, None), refresh_per_second=12, transient=True)
 
         my_loadcell.start_reading()
         _, _, t0 = my_controller.run(speed, displacement, direction)
@@ -54,10 +56,12 @@ def _run_go(my_controller: controller.LinearController, my_loadcell: loadcell.Lo
                     my_controller.abort()
                 else:
                     while my_loadcell.is_batch_ready(batch_index):
+                        if (plot_data.getData()[1] is not None) and (len(plot_data.getData()[1]) > 500):
+                            plot_data, forces, strains = _init_plot_data(plot_item, plot_color)
+
                         batch, batch_index = my_loadcell.get_batch(batch_index)
                         batch['t'] = batch['t'] - t0
-                        batch['strain'] = ((batch['t'] * speed + reference_absolute_position -
-                                           initial_absolute_position) / initial_gauge_length) * 100
+                        batch['strain'] = ((batch['t'] * speed + reference_absolute_position - initial_absolute_position) / initial_gauge_length) * 100
 
                         forces.extend(batch['F'])
                         strains.extend(batch['strain'])
@@ -71,8 +75,7 @@ def _run_go(my_controller: controller.LinearController, my_loadcell: loadcell.Lo
                     live_table.update(
                         helpers.generate_data_table(
                             force=forces[-1] if len(forces) > 0 else None,
-                            absolute_position=(initial_absolute_position + (
-                                strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
+                            absolute_position=(initial_absolute_position + (strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
                             loadcell_limit=loadcell_limit,
                             force_offset=my_loadcell.get_offset(is_force=True),
                             test_parameters=test_parameters
@@ -85,21 +88,17 @@ def _run_go(my_controller: controller.LinearController, my_loadcell: loadcell.Lo
 
         # DERIVED DATA COMPUTATION
         t0 = data['t'][0]
-        data['displacement'] = (data['t'] - t0) * speed + \
-            (reference_absolute_position - initial_absolute_position)
-        data['strain'] = (data['displacement'] /
-                          test_parameters['initial_gauge_length']['value']) * 100
+        data['displacement'] = (data['t'] - t0) * speed + (reference_absolute_position - initial_absolute_position)
+        data['strain'] = (data['displacement'] / test_parameters['initial_gauge_length']['value']) * 100
 
     return data, stop_flag
 
-
-def _run_delay(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, plot_item, plot_color, delay, stop_flag, stop_button, initial_absolute_position, test_parameters):
+def _run_delay(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, plot_item, plot_color, delay, stop_flag, stop_button, initial_absolute_position, test_parameters):
     data = None
 
     if stop_flag is False and delay != 0:
         initial_gauge_length = test_parameters['initial_gauge_length']['value']
-        loadcell_limit = my_loadcell.get_calibration()[
-            'loadcell_limit']['value']
+        loadcell_limit = my_loadcell.get_calibration()['loadcell_limit']['value']
 
         stop_flag = stop_flag
 
@@ -109,20 +108,12 @@ def _run_delay(my_controller: controller.LinearController, my_loadcell: loadcell
             return
         stop_button.when_released = lambda: _switch_stop_flag()
 
-        plot_data = plot_item.plot(
-            pen=None, symbol=constants.PLOTS_SYMBOL, symbolSize=constants.PLOTS_SYMBOL_SIZE)
-        plot_data.opts['useCache'] = True
-        plot_data.setSymbolPen(mkPen(plot_color))
-
-        strains = []
-        forces = []
+        plot_data, forces, strains = _init_plot_data(plot_item, plot_color)
         batch_index = 0
 
-        fixed_strain = ((my_controller.get_absolute_position(
-        ) - initial_absolute_position) / initial_gauge_length) * 100
+        fixed_strain = ((my_controller.get_absolute_position() - initial_absolute_position) / initial_gauge_length) * 100
 
-        live_table = Live(helpers.generate_data_table(
-            None, None, None, None), refresh_per_second=12, transient=True)
+        live_table = Live(helpers.generate_data_table(None, None, None, None), refresh_per_second=12, transient=True)
 
         my_loadcell.start_reading()
         t0 = my_controller.hold_torque()
@@ -133,6 +124,9 @@ def _run_delay(my_controller: controller.LinearController, my_loadcell: loadcell
                     my_controller.release_torque()
                 else:
                     while my_loadcell.is_batch_ready(batch_index):
+                        if (plot_data.getData()[1] is not None) and (len(plot_data.getData()[1]) > 500):
+                            plot_data, forces, strains = _init_plot_data(plot_item, plot_color)
+                        
                         batch, batch_index = my_loadcell.get_batch(batch_index)
                         batch['t'] = batch['t'] - t0
                         batch['strain'] = fixed_strain
@@ -149,8 +143,7 @@ def _run_delay(my_controller: controller.LinearController, my_loadcell: loadcell
                     live_table.update(
                         helpers.generate_data_table(
                             force=forces[-1] if len(forces) > 0 else None,
-                            absolute_position=(initial_absolute_position + (
-                                strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
+                            absolute_position=(initial_absolute_position + (strains[-1] * initial_gauge_length / 100)) if len(strains) > 0 else None,
                             loadcell_limit=loadcell_limit,
                             force_offset=my_loadcell.get_offset(is_force=True),
                             test_parameters=test_parameters
@@ -166,7 +159,6 @@ def _run_delay(my_controller: controller.LinearController, my_loadcell: loadcell
         data['strain'] = fixed_strain
 
     return data, stop_flag
-
 
 def _start_monotonic_test(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, test_parameters: dict, stop_button_pin: int):
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...')
@@ -186,8 +178,7 @@ def _start_monotonic_test(my_controller: controller.LinearController, my_loadcel
     plot_widget.setMouseEnabled(x=False, y=False)
     plot_item = plot_widget.getPlotItem()
 
-    xlim = round((displacement / initial_gauge_length)
-                 * 1.1 * 100)  # 10% margin
+    xlim = round((displacement / initial_gauge_length) * 1.1 * 100)  # 10% margin
     ylim = loadcell_limit
     plot_item.getViewBox().setRange(xRange=(0, xlim), yRange=(0, ylim))
     plot_item.setLabel('bottom', 'Strain', '%')
@@ -210,20 +201,16 @@ def _start_monotonic_test(my_controller: controller.LinearController, my_loadcel
     )
 
     utility.delete_last_lines(printed_lines)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
 
-    console.print('[#e5c07b]>[/#e5c07b]',
-                  'Waiting for the plot figure to be closed...')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...')
     pg.exec()
     utility.delete_last_lines(n_lines=1)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...', '[green]:heavy_check_mark:[/green]')
 
     return data
 
-
-def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, test_parameters: dict, stop_button_pin: int):
+def _start_cyclic_test(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, test_parameters:dict, stop_button_pin: int):
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...')
     printed_lines = 1
 
@@ -264,8 +251,7 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
     plot_item = plot_widget.getPlotItem()
     plot_color_idx = 0
 
-    xlim = round((cyclic_upper_limit / initial_gauge_length)
-                 * 1.1 * 100)  # 10% margin
+    xlim = round((cyclic_upper_limit / initial_gauge_length) * 1.1 * 100)  # 10% margin
     ylim = loadcell_limit
     plot_item.getViewBox().setRange(xRange=(0, xlim), yRange=(0, ylim))
     plot_item.setLabel('bottom', 'Strain', '%')
@@ -286,8 +272,7 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
             plot_item=plot_item,
             plot_color=constants.PLOT_COLORS_LIST[plot_color_idx],
             speed=pretensioning_speed,
-            displacement=(
-                cyclic_upper_limit + initial_absolute_position) - reference_absolute_position,
+            displacement=(cyclic_upper_limit + initial_absolute_position) - reference_absolute_position,
             direction=controller.UP,
             stop_flag=stop_flag,
             stop_button=stop_button,
@@ -366,8 +351,7 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
             plot_item=plot_item,
             plot_color=constants.PLOT_COLORS_LIST[plot_color_idx],
             speed=cyclic_speed,
-            displacement=(
-                cyclic_upper_limit + initial_absolute_position) - reference_absolute_position,
+            displacement=(cyclic_upper_limit + initial_absolute_position) - reference_absolute_position,
             direction=controller.UP,
             stop_flag=stop_flag,
             stop_button=stop_button,
@@ -394,8 +378,7 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
         )
         if data is not None:
             data_list.append(data)
-            data_labels_list.append(
-                'cycle_' + str(cycle_idx) + '_return_delay')
+            data_labels_list.append('cycle_' + str(cycle_idx) + '_return_delay')
 
         # CYCLIC PHASE - RETURN
         plot_color_idx += 1
@@ -406,8 +389,7 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
             plot_item=plot_item,
             plot_color=constants.PLOT_COLORS_LIST[plot_color_idx],
             speed=cyclic_return_speed,
-            displacement=reference_absolute_position -
-            (initial_absolute_position + cyclic_lower_limit),
+            displacement=reference_absolute_position - (initial_absolute_position + cyclic_lower_limit),
             direction=controller.DOWN,
             stop_flag=stop_flag,
             stop_button=stop_button,
@@ -481,18 +463,14 @@ def _start_cyclic_test(my_controller: controller.LinearController, my_loadcell: 
             data_labels_list.append('failure_go')
 
     utility.delete_last_lines(printed_lines)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
 
-    console.print('[#e5c07b]>[/#e5c07b]',
-                  'Waiting for the plot figure to be closed...')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...')
     pg.exec()
     utility.delete_last_lines(n_lines=1)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...', '[green]:heavy_check_mark:[/green]')
 
     return data_list, data_labels_list
-
 
 def _start_static_test(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, stop_button_pin: int):
     console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...')
@@ -521,8 +499,7 @@ def _start_static_test(my_controller: controller.LinearController, my_loadcell: 
     plot_item.setLabel('left', 'Force', 'N')
     plot_item.setTitle('Force vs. Time')
 
-    plot_data = plot_item.plot(
-        pen=None, symbol=constants.PLOTS_SYMBOL, symbolSize=constants.PLOTS_SYMBOL_SIZE)
+    plot_data = plot_item.plot(pen=None, symbol=constants.PLOTS_SYMBOL, symbolSize=constants.PLOTS_SYMBOL_SIZE)
     plot_data.opts['useCache'] = True
     plot_data.setSymbolPen(mkPen(constants.PLOT_COLORS_LIST[0]))
 
@@ -530,8 +507,7 @@ def _start_static_test(my_controller: controller.LinearController, my_loadcell: 
     forces = []
     batch_index = 0
 
-    live_table = Live(helpers.generate_data_table(None, None, None,
-                      None), refresh_per_second=12, transient=True)
+    live_table = Live(helpers.generate_data_table(None, None, None, None), refresh_per_second=12, transient=True)
 
     t0 = my_controller.hold_torque()
     my_loadcell.start_reading()
@@ -569,18 +545,15 @@ def _start_static_test(my_controller: controller.LinearController, my_loadcell: 
                 )
 
     utility.delete_last_lines(printed_lines)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Collecting data...', '[green]:heavy_check_mark:[/green]')
 
     data = my_loadcell.stop_reading()
     stop_button.when_released = None
 
-    console.print('[#e5c07b]>[/#e5c07b]',
-                  'Waiting for the plot figure to be closed...')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...')
     pg.exec()
     utility.delete_last_lines(n_lines=1)
-    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Waiting for the plot figure to be closed...', '[green]:heavy_check_mark:[/green]')
 
     data['t'] = data['t'] - t0
     data['F_raw'] = data['F']
@@ -588,8 +561,7 @@ def _start_static_test(my_controller: controller.LinearController, my_loadcell: 
 
     return data
 
-
-def start_test(my_controller: controller.LinearController, my_loadcell: loadcell.LoadCell, test_parameters: dict, test_dir: str, stop_button_pin: int):
+def start_test(my_controller:controller.LinearController, my_loadcell:loadcell.LoadCell, test_parameters:dict, test_dir:str, stop_button_pin:int):
     data = None
     data_labels = None
 
@@ -618,8 +590,7 @@ def start_test(my_controller: controller.LinearController, my_loadcell: loadcell
         if data is not None:
             data = helpers.postprocess_data(data, test_parameters)
 
-    console.print('[#e5c07b]>[/#e5c07b]', 'Postprocessing test data...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Postprocessing test data...', '[green]:heavy_check_mark:[/green]')
 
     with console.status('Saving test data...'):
         # Save .xlsx file
@@ -631,11 +602,9 @@ def start_test(my_controller: controller.LinearController, my_loadcell: loadcell
             if isinstance(data, list):
                 data_list = data
                 for idx, _ in enumerate(data_list):
-                    data_list[idx].to_excel(
-                        writer, sheet_name=data_labels[idx], index=False)
+                    data_list[idx].to_excel(writer, sheet_name=data_labels[idx], index=False)
             else:
-                data.to_excel(
-                    writer, sheet_name=test_parameters['test_id'], index=False)
+                data.to_excel(writer, sheet_name=test_parameters['test_id'], index=False)
 
             writer.save()
 
@@ -648,7 +617,6 @@ def start_test(my_controller: controller.LinearController, my_loadcell: loadcell
 
             data.to_csv(os.path.join(test_dir, filename), index=False)
 
-    console.print('[#e5c07b]>[/#e5c07b]', 'Saving test data...',
-                  '[green]:heavy_check_mark:[/green]')
+    console.print('[#e5c07b]>[/#e5c07b]', 'Saving test data...', '[green]:heavy_check_mark:[/green]')
 
     return
